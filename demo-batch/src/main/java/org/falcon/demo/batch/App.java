a1a6eea73fb8ff2636d40cc6a4c287bbfb2f3ddb
fouadGeek <elayachifouad@gmail.com>
2020-10-06 20:41:36 +0100

    Add the version property for the batch and webapp modules and filter it while building the project.

--- demo-batch/src/main/java/org/falcon/demo/batch/App.java
+++ demo-batch/src/main/java/org/falcon/demo/batch/App.java
@@ -1,13 +1,16 @@
 package org.falcon.demo.batch;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
